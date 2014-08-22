8db6cc217e63abfc2fb2c474d0d11c19683372dd
jennyjnl <jenny_jnl@hotmail.com>
2014-08-22 12:04:34 +0200

    add accent

--- affinitic/smsutils/send_sms.py
+++ affinitic/smsutils/send_sms.py
@@ -9,12 +9,6 @@ récupération des informations fournie via terminal
 (section = groupe def dans sms.cfg - message = "le message txt") """
 
 
-class phone_numbers():
-    def __init__(self, section, message):
-        self.section = section
-        self.message = message
-
-
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--section', required=True)
@@ -25,6 +19,7 @@ def get_args():
 
 def send_sms(section, message, sender=None):
 
+    message = message.decode('utf-8')
     config = ConfigParser.RawConfigParser()
     sms_config_path = os.environ['SMS_CONFIG_PATH']
     config.read(sms_config_path)
@@ -37,7 +32,7 @@ def send_sms(section, message, sender=None):
 
     for nom, number in section:
         if sender != number:
-            message_send = {'Text': 'Hello {0}.\n{1}'.format(nom, message), 'SMSC': {'Location': 1}, 'Number': number}
+            message_send = {'Text': u'Hello {0}.\n{1}'.format(nom, message), 'SMSC': {'Location': 1}, 'Number': number}
             smsd.InjectSMS([message_send])
 
 
