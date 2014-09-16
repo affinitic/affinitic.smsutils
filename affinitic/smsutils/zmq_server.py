ce7f2c624411eba30b2e76a9f7fff049350536f4
EZPK <thom.bruyer@gmail.com>
2014-09-16 10:26:50 +0200

    Envoi des messages du client vers IRC

--- affinitic/smsutils/zmq_server.py
+++ affinitic/smsutils/zmq_server.py
@@ -4,14 +4,16 @@
 #   Expects "Hello" from client, replies with "World"
 #
 import zmq
+from subprocess import Popen
 
 
 def main():
     context = zmq.Context()
     socket = context.socket(zmq.REP)
-    socket.bind("tcp://192.168.99.121:5555")
+    socket.bind("tcp://192.168.99.115:5555")
 
     while True:
         message = socket.recv()
         print("Received request: %s" % message)
-        socket.send("World")
+        Popen(message.split('|'))
+        socket.send("World")    
